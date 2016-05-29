7008bd8aee058f6000d7a3acf7f77e97566c9112
0xd34df00d <0xd34df00d@gmail.com>
2016-05-29 15:02:50 -0400

    Updated Converter.hs.

--- converter/Converter.hs
+++ converter/Converter.hs
@@ -1,14 +1,18 @@
+import qualified Data.Vector as V
+import qualified Data.Text as T
+import qualified Data.Text.IO as TI
 import qualified Data.ByteString.Lazy.Char8 as BS
 import System.Environment
 
+import Node
 import Loader
+import ImageRefExtractor
+import Html2Markdown
 
-process :: [String] -> IO ()
+--process :: [String] -> IO ()
 process files | length files == 3 = do
     datas <- mapM BS.readFile files
-    let res = processFiles datas
-    return ()
-              | otherwise = putStrLn "Usage"
+    return $ processFiles datas
 
-main :: IO ()
+--main :: IO ()
 main = getArgs >>= process
