e9c2803422127e62c2ab90ecabb26ed88738fca9
0xd34df00d <0xd34df00d@gmail.com>
2016-05-29 13:58:41 -0400

    Properly handle unknown HTML.

--- converter/Html2Markdown.hs
+++ converter/Html2Markdown.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Html2Markdown where
 
 import qualified Data.Text as T
 import qualified Text.HTML.TagSoup as TS
 import qualified Text.HTML.TagSoup.Tree as TS.T
+import Data.Monoid
 import Data.Char
 import Data.Maybe
 
 convert :: T.Text -> _ 
-convert = --mconcat . map toMd .
+convert = mconcat . map toMd .
             cleanupHtml . TS.T.parseTree
 
 cleanupHtml :: [TS.T.TagTree T.Text] -> [TS.T.TagTree T.Text]
@@ -24,4 +26,8 @@ toMd :: TS.T.TagTree T.Text -> T.Text
 toMd (TS.T.TagLeaf (TS.TagText t)) = t
 toMd (TS.T.TagBranch "strong" [] cs) = mconcat $ "*" : map toMd cs ++ ["*"]
 toMd (TS.T.TagBranch "br" [] []) = "\n"
+toMd (TS.T.TagBranch n attrs cs) | null cs = tmpl
+                                 | T.count "<" tmpl /= 2 = error "Nowhere to insert the children" 
+                                 | (h, t) <- T.breakOnEnd "<" tmpl = T.init h <> (mconcat $ map toMd cs) <> (T.last h `T.cons` t)
+    where tmpl = TS.T.renderTree [TS.T.TagBranch n attrs []]
 toMd t = TS.T.renderTree [t]
