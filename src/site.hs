781e00c2e1e66e128804d4dc5c0c43f90ac51fb2
0xd34df00d <0xd34df00d@gmail.com>
2016-12-17 00:18:21 -0500

    [lcss] Removed some old stock Hakyll rules.

--- src/site.hs
+++ src/site.hs
@@ -49,41 +49,6 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/plugins.html" pluginsCtx
                 >>= loadAndApplyTemplate "templates/default.html" pluginsCtx
                 >>= relativizeUrls
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
 
     match "templates/*" $ compile templateBodyCompiler
 
